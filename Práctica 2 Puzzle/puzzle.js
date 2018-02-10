b879fff230b0915765f35502e37e097589dbebd9
miguelangel3 <miguelangel.abb@gmail.com>
2018-02-10 23:13:22 +0100

    Modificaciones para intentar borrar la imange de fondo

--- Práctica 2 Puzzle/puzzle.js
+++ Práctica 2 Puzzle/puzzle.js
@@ -2,9 +2,18 @@ function prinTime(){
     var d = new Date();
     document.getElementById('time').innerHTML = "Time:" + d.toLocaleTimeString();
 }
+
+function matrizImagen(){
+    var matrix = new array();
+    matrix.id = "im";
+    matrix.x = 0.0;
+    matirx.y = 0.0;
+}
 function recortarImagen(image){
     ctx.drawImage(image,0,0,200,200);
+    ctx.clearRect(0,0,50,50);
     //ctx.drawImage(image,0,0,canvas.width,canvas.height);
+    //Duda si borro la imagen también borro la recortada
     var recorteImagen = ctx.getImageData(0,0,133.3,133.3);
     ctx.putImageData(recorteImagen,100,100);
 }
