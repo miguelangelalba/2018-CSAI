6ec9d3bfd004f2659534d3be66f460457ed922d7
miguelangel3 <miguelangel.abb@gmail.com>
2018-02-10 21:31:44 +0100

    Aprendiendo a recortar imagenes

--- Práctica 2 Puzzle/puzzle.js
+++ Práctica 2 Puzzle/puzzle.js
@@ -2,8 +2,22 @@ function prinTime(){
     var d = new Date();
     document.getElementById('time').innerHTML = "Time:" + d.toLocaleTimeString();
 }
+function recortarImagen(image){
+    ctx.drawImage(image,0,0,200,200);
+    //ctx.drawImage(image,0,0,canvas.width,canvas.height);
+    var recorteImagen = ctx.getImageData(0,0,133.3,133.3);
+    ctx.putImageData(recorteImagen,100,100);
+}
 
 function main(){
-var arrayImages = [tesla,cohete,praga];
+var canvas = document.getElementById('canvas');
+ctx = canvas.getContext("2d");
+
+var arrayImages = ["tesla","cohete","praga"];
 var myVar = setInterval(prinTime,1000);
+
+var image = new Image();
+image.src = "Images/cohete.jpg";
+recortarImagen(image);
+
 }
