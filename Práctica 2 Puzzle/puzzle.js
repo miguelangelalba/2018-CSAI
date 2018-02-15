9dc539de459a41db43d658ebc818e4c79576a5d4
miguelangel3 <miguelangel.abb@gmail.com>
2018-02-15 18:17:36 +0100

    Galería casi comletada

--- Práctica 2 Puzzle/puzzle.js
+++ Práctica 2 Puzzle/puzzle.js
@@ -1,4 +1,5 @@
-var slideIndex = 0
+var slideIndex = 1;
+
 function prinTime(){
     var d = new Date();
     document.getElementById('time').innerHTML = "Time:" + d.toLocaleTimeString();
@@ -24,13 +25,48 @@ function recortarImagen(src){
     ctx.putImageData(recorteImagen,100,100);
 }
 function showSlides(){
+    console.log("cosa");
     var i;
     var slides = document.getElementsByClassName("myImages");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";
     }
+
+
+
+    console.log(slideIndex);
+    console.log(slides.length);
+    switch(slideIndex) {
+
+        case 0 :
+            //document.getElementById("img1").src = "Images/tesla.jpg";
+        //    document.getElementById("img2").src = "Images/cohete.jpg";
+        //    document.getElementById("img3").src = "Images/praga.jpg";
+        //    break;
+        case 1:
+            document.getElementById("img1").src = "Images/cohete.jpg";
+            document.getElementById("img2").src = "Images/tesla.jpg";
+            document.getElementById("img3").src = "Images/praga.jpg";
+            break;
+        case 2:
+            document.getElementById("img1").src = "Images/praga.jpg";
+            document.getElementById("img2").src = "Images/impresora3d.jpg";
+            document.getElementById("img3").src = "Images/dodecaedro.jpg";
+            break;
+        case 3:
+        document.getElementById("img1").src = "Images/tesla.jpg";
+        document.getElementById("img2").src = "Images/cohete.jpg";
+        document.getElementById("img3").src = "Images/praga.jpg";
+
+            break;
+        //case n:
+        //    code block
+        //    break;
+
+        }
     slideIndex++;
+
     if (slideIndex > slides.length) {slideIndex = 1}
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
