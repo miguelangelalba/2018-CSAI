e1b4afd87837610f64e1a4db89b98ecc932634e9
miguelangel3 <miguelangel.abb@gmail.com>
2018-02-20 15:03:37 +0100

    Empiezo a definir la estructura de mi puzzle con objetos. Ahora mismo tengo un problema en el js. No pilla bien las constantes del principio

--- Práctica 2 Puzzle/puzzle.js
+++ Práctica 2 Puzzle/puzzle.js
@@ -1,4 +1,46 @@
+//Tamaño canvas fijo 500x500
+
+//falla aquí tengoq que ver que le pasa
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
 var slideIndex = 1;
+
+function pieza(image,sx,sy,dy,dx,swidht,sheight,pox,poy,px,py){
+    this.image = image;
+    this.sX = sx;
+    this.sY = sy;
+    this.dY = dy;
+    this.dX = dx;
+    this.sWidht = swidht;
+    this.sHeight = sheight;
+    //En este caso dWith y dHeight valen lo mismo
+    //En esta parte defino mi propio tablero y organizo según mis coordeanadas
+    //de tablero.
+    this.positionOriginalX = pox;
+    this.positionOriginalY = poy;
+    this.myPositionX = px;
+    this.myPositionY = py;
+
+    this.drawFicha = function(){
+        ctx.drawImage(image,this.sX,this.sY,this.sWidht,this.sHeight,this.dX,this.dY,this.sWidht,this.sHeight);
+
+    }
+    this.changePosition = function(){
+        this.positionX += 1;
+        this.positionY += 1;
+    }
+
+}
+
+function resize(image){
+//con esta función redimensiono la imagen para no tener problemas de dónde me
+//las descargo
+    imgResize = image;
+    imgResize.width = 500;
+    imgResize.height = 500;
+    return imgResize;
+}
 
 function prinTime(){
     var d = new Date();
@@ -25,7 +67,6 @@ function recortarImagen(src){
     ctx.putImageData(recorteImagen,100,100);
 }
 function showSlides(){
-    console.log("cosa");
     var i;
     var slides = document.getElementsByClassName("myImages");
     var dots = document.getElementsByClassName("dot");
@@ -33,10 +74,6 @@ function showSlides(){
        slides[i].style.display = "none";
     }
 
-
-
-    console.log(slideIndex);
-    console.log(slides.length);
     switch(slideIndex) {
 
         case 0 :
@@ -55,9 +92,9 @@ function showSlides(){
             document.getElementById("img3").src = "Images/dodecaedro.jpg";
             break;
         case 3:
-        document.getElementById("img1").src = "Images/tesla.jpg";
-        document.getElementById("img2").src = "Images/cohete.jpg";
-        document.getElementById("img3").src = "Images/praga.jpg";
+            document.getElementById("img1").src = "Images/tesla.jpg";
+            document.getElementById("img2").src = "Images/cohete.jpg";
+            document.getElementById("img3").src = "Images/praga.jpg";
 
             break;
         //case n:
@@ -76,9 +113,20 @@ function showSlides(){
     setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
 
+function makePuzzle(puzzle){
+
+    for (var i = 0; i < 8; i++){
+        //me quedaría hacer otro bucle para pasar las posiciones
+        puzzle.push(new pieza())
+    }
+
+}
+
 function main(){
-var canvas = document.getElementById('canvas');
-ctx = canvas.getContext("2d");
+//var canvas = document.getElementById('canvas');
+//ctx = canvas.getContext("2d");
+var puzzle = new Array();
+
 showSlides();
 //var myVar = setInterval(prinTime,1000);
 
